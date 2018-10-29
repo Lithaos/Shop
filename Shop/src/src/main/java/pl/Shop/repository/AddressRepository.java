6b44a3cae52d57e9dea03076db9220a71de0c661
Wojciech Raczyński <wojto3@op.pl>
2018-10-29 19:36:09 +0100

    Delete AddressRepository.java

--- Shop/src/src/main/java/pl/Shop/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.Shop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import pl.Shop.model.User;
-
-public interface AddressRepository extends JpaRepository<User, Long> {
-
-}
