435eff979781417d4f26f97d5da32cadeecadbc1
Wojciech Raczyński <wojto3@op.pl>
2018-10-29 19:36:06 +0100

    Delete CartRepository.java

--- Shop/src/src/main/java/pl/Shop/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.Shop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import pl.Shop.model.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-
-}
